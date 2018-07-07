0f501bdf914b60f694b871df09266de53c78175b
PrincessYork <york.git@gmail.com>
2018-07-07 23:13:06 +0300

    no message

---
+++ app/src/main/java/com/egoralmikeev/navigation/ThirdFragment.java
@@ -0,0 +1,38 @@
+package com.egoralmikeev.navigation;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.navigation.Navigation;
+
+
+public class ThirdFragment extends Fragment {
+
+    public ThirdFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_third, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        String fromWhere = ThirdFragmentArgs.fromBundle(getArguments()).getFromWhere();
+        Toast.makeText(getActivity(), fromWhere, Toast.LENGTH_SHORT).show();
+    }
+}
